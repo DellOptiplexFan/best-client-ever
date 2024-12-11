90ad471de0ce44d6e22aa78dcb867c7101ec2fc9
ufyu <80692105+ufyo@users.noreply.github.com>
2024-12-11 10:43:29 -0500

    here u go

--- src/client/java/com/example/modules/Movement/Eagle.java
+++ src/client/java/com/example/modules/Movement/Eagle.java
@@ -4,24 +4,30 @@ import com.example.modules.Mod;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.entity.player.PlayerEntity;
 import org.lwjgl.glfw.GLFW;
 
 public class Eagle extends Mod {
 
     public Eagle() {
         super("Eagle", "Sneaks on the edges of blocks.", Category.MOVEMENT);
-        this.setKey(GLFW.GLFW_KEY_V);
+        this.setKey(GLFW.GLFW_KEY_V); 
     }
+
     @Override
     public void onTick() {
+        if (mc.player == null || mc.world == null) {
+            return; // exit dis bih
+        }
+
         BlockPos playerPos = mc.player.getBlockPos();
-        BlockPos blockPelowPos = playerPos.down();
-        BlockState blockState = mc.world.getBlockState(blockPelowPos);
-        if (blockState.isAir()) {
-            mc.player.setSneaking(true);
-        } else {
-            mc.player.setSneaking(false);
+        BlockPos blockBelowPos = playerPos.down();
+
+        BlockState blockStateBelow = mc.world.getBlockState(blockBelowPos);
+
+        boolean shouldSneak = blockStateBelow.isAir();
+
+        if (mc.player.isSneaking() != shouldSneak) {
+            mc.player.setSneaking(shouldSneak);
         }
     }
 

5f90725687fea94c331018eee5649781531703ab
DellOptiplexFan <134756793+DellOptiplexFan@users.noreply.github.com>
2024-12-11 09:20:42 -0800

    Create ClickGUI.java

---
+++ src/client/java/com/example/modules/Render/ClickGUI.java
@@ -0,0 +1,18 @@
+package com.example.modules.Render;
+
+import com.example.modules.Mod;
+import org.lwjgl.glfw.GLFW;
+
+public class ClickGUI extends Mod {
+
+    public ClickGUI() {
+        super("ClickGUI", "Clicks the GUI", Category.RENDER);
+    }
+    @Override
+    public void onTick() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}

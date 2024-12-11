146986c32c8e170ff3e932cba35fcb847ffcf123
DellOptiplexFan <134756793+DellOptiplexFan@users.noreply.github.com>
2024-12-11 11:08:53 -0800

    FUCK

--- src/client/java/com/example/modules/ui/screens/ClickGUI.java
+++ src/client/java/com/example/modules/ui/screens/ClickGUI.java
@@ -1,5 +1,12 @@
 package com.example.modules.ui.screens;
 
+import imgui.ImGui;
+import net.minecraft.network.chat.Component;
+import xyz.breadloaf.imguimc.screen.ImGuiScreen;
+import xyz.breadloaf.imguimc.screen.ImGuiWindow;
+import xyz.breadloaf.imguimc.theme.ImGuiDarkTheme;
+
+import java.util.List;
 
 public class ClickGUI {
 
